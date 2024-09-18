d4ca0d775c4bfb5cde17a9c6c2509dd16cd5df58
mazen14200 <mazen142000@yahoo.com>
2024-09-18 14:38:08 +0300

    commit_7 added timeout to all endpoints

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ app.use(cors())
 const port = 3000||process.env.port
 const bodyParser = require('body-parser')
 
+app.use((req, res, next) => {
+  const timeout = 30000; // 30000 مللي ثانية = 30 ثواني
+  res.setTimeout(timeout, () => {
+      console.log('Request has timed out.');
+      res.status(503).send({status:"false",response:'Request timed out.'});
+  });
+
+  next();
+});
+
 const router = require("./routers/wa");
 app.use(bodyParser.json());
 app.use("/",router);
