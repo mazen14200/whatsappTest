4553a0dca755bcea05f4ef4b304242e8bcf0dbc9
mazen14200 <mazen142000@yahoo.com>
2024-09-18 17:20:25 +0300

    commit_15

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-//const cors = require('cors')
+const cors = require('cors')
 
 const app = express()
 
-//app.use(cors())
+app.use(cors())
 const port = process.env.port || 3000
 const bodyParser = require('body-parser')
 
