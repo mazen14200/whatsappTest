8e9892a07fe1152f13c286a26fa25c65ea5ebc4e
mazen14200 <mazen142000@yahoo.com>
2024-09-22 15:57:24 +0300

    commit_20

--- routers/wa.js
+++ routers/wa.js
@@ -9,7 +9,7 @@ const router = express.Router();
 /*router.get("/api", (req,res)=>{
     res.json({status : "OK" });
 });*/
-router.get("/generateQrCode", generateQrCode);
+router.get("/api/generateQrCode", generateQrCode);
 router.get("/api/sendMedia", sendMedia);
 router.post("/api/sendMedia", sendMedia);
 router.get("/api/sendMessage_text", sendMessage_text);
